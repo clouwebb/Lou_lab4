5b2edf3b3fa818b207cb1b9c591eb062ec0a395d
clouwebb <76088315+clouwebb@users.noreply.github.com>
2021-01-10 18:49:46 -0800

    Update myfile.py
    I realized that I have been journaling wrong. But for the last few sessions, I have been working during lunch and after dinner. I did 30 minutes from 11-11:30am, and at night I usually work from 6 - 6:40pm.  I try to work for 70 minutes (because we are required to). But I just do not know what is going on with my errors. However, I feel like I am extremely close. I do apologize for submitting more than 7 entries.

--- myfile.py
+++ myfile.py
@@ -18,22 +18,23 @@ def selectsort (dnaseq):
     numseq = []
     
     while len (dnaseq) > 0:
+
         for numseq in dnaseq:
+#the code below should be saying that if there is and A in the dna sequence, then i will make a newA that adds up to the old A
             if dnaseq(A) = A:
                 newA = A + 1
             if dnaseq(G) = G:
                 newG = G + 1
-            if dnaseq(C) = C
+            if dnaseq(C) = C:
                 newC = C + 1
-            if dnaseq(T) = T
+            if dnaseq(T) = T:
                 newT = T + 1
-    else 
-        return (newA, newG, newC, newT)
+#i will return the "new" number and it should be an integer
+            return (newA, newG, newC, newT)
 
 if __name__ == "__main__":
     dnaseq = [AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC]
     selectsort (dnaseq)
     print (numseq)
-
 
 
