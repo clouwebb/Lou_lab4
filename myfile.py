fc341ccbabe9c4dbecf2c65176f27198de1c031a
clouwebb <76088315+clouwebb@users.noreply.github.com>
2021-01-13 14:23:38 -0800

    Update myfile.py
    Today is Wednesday January 13th. I started to debug problem 1 and started on problem 2. I think I am close to being done for both but there a little mistakes here and there that I cannot find. I worked from 1:30pm to 2:20pm. Then I continued from 3:00pm - 3:30pm. It does not look like I did much, but I just kept trying to fix problem 1 but every change makes it a bit more complicated. I also tried to google an explanation for problem 3 as I did not understand it. But it seems like everyone is using Java instead of Visual Studio Code.
    
    I do not know what is wrong with problem 2, because I took a lot of inspiration from our lab 2 where we did the reverse. However, I am only transcribing the letter T and I do not know if I need a place holder for that.
    
    I also took a look at problem 3, but I see no pattern there to make a loop for. It was similar related to pairings but I am unsure about that. I already made an office hours appointment for thursday evening, where I will be asking Ms. DeRanek to debug problems 1 and 2 with me and also ask her to extend on problem 3 so I know how to continue it.

--- myfile.py
+++ myfile.py
@@ -10,6 +10,7 @@
 #make a selecttion sort function
 def selectsort (dnaseq):
 
+#this is how i make the letters integers
     A = 0
     G = 0
     C = 0
@@ -19,22 +20,53 @@ def selectsort (dnaseq):
     
     while len (dnaseq) > 0:
 
-        for numseq in dnaseq:
+        for letter in dnaseq:
 #the code below should be saying that if there is and A in the dna sequence, then i will make a newA that adds up to the old A
-            if dnaseq(A) = A:
-                newA = A + 1
-            if dnaseq(G) = G:
-                newG = G + 1
-            if dnaseq(C) = C:
-                newC = C + 1
-            if dnaseq(T) = T:
-                newT = T + 1
+
+            if letter = A:
+                newA += 1
+                newA = newA + 1
+            if letter = G:
+                newG += 1
+                newG = newG + 1
+            if letter = C:
+                newC += 1
+                newC = newC + 1
+            if letter = T:
+                newT += 1
+                newT = newT + 1
+                
 #i will return the "new" number and it should be an integer
             return (newA, newG, newC, newT)
 
+#calling
 if __name__ == "__main__":
     dnaseq = [AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC]
     selectsort (dnaseq)
     print (numseq)
+    
+#Problem 2
+#Catherine
+#RNA is A, G, C, U
+#transcribe the T to U
+
+#input
+#GATGGAACTTGACTACGTAAATT
+#Output 
+#GAUGGAACUUGACUACGUAAAUU
+
+def transcribe (dnaseq):
+
+
+    while len(dnaseq) > 0:
+        
+        rna = dnaseq[0:]
+        rna = rna.replace ("T", "U")
+
+if __name__ == "__main__":
+    dnaseq = ["GATGGAACTTGACTACGTAAATT"]
+    transcribe (dnaseq)
+    print (dnaseq)
+
 
 
