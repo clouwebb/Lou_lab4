dc46b6823ca2676fefee8189fee38097e071a33b
clouwebb <76088315+clouwebb@users.noreply.github.com>
2021-01-24 19:28:59 -0800

    Update myfile.py
    Final touches

--- myfile.py
+++ myfile.py
@@ -16,13 +16,9 @@ def selectsort (dnaseq):
     T = 0
     
 #i did not need the while loop because the string is never going to change in length
-
-    for letter in dnaseq:
-        
+    for letter in dnaseq:       
 #if i want to compare the letters, i need to make it a string because and A does not equal the interger A
-
-        if letter == 'A':
-        
+        if letter == 'A':    
 #removed the newA because it becomes conditional to whether i actually see A or not. If i dont then newA becomes useless
 
             A = A + 1
@@ -55,11 +51,8 @@ if __name__ == "__main__":
 def transcribe (dnaseq):
 
 #running from the front all the way to the end
-
-    T = dnaseq[0:]
-    
+    T = dnaseq[0:]    
 #this is the newstring replacing the T with U
-
     dnaseq = T.replace ("T", "U")
     print (dnaseq)
     
@@ -82,17 +75,13 @@ if __name__ == '__main__':
 def reverse(dnaseq):
 
 #this is making the computer read the list backwards
-
     reverse = dnaseq[::-1]
-    
 #this is individually changing the code by having a "temp"
-
     reverse = reverse.replace('A', 't')
     reverse = reverse.replace('T', 'a')
     reverse = reverse.replace('G', 'c')
     reverse = reverse.replace('C', 'g')
-    reverse = reverse.upper() 
-    
+    reverse = reverse.upper()    
 #printing out the new statement
 
     print (reverse)
@@ -113,27 +102,19 @@ if __name__ == "__main__":
 #output
 
 def hamming (dnaseq1, dnaseq2):
-    
+   
 # Return the Hamming distance between dnaseq1 and dnaseq2.
-
     hammingdistance = 0
-
     DNASEQ1 = len(dnaseq1)
-
     for i in range(DNASEQ1):
         
 #telling my computer to add one to the "hammingdistance" variable if there is one that doesnt match
-
         if dnaseq1[i] != dnaseq2[i]:
-        
 #in addition to adding one, also look past it and scan the next letter
-
-            hammingdistance += 1
-    
+            hammingdistance += 1   
 #return the final hamming distance
 
     return hammingdistance
-
 
 if __name__ == "__main__":
     finaldist = hamming('GAGCCTACTAACGGGAT','CATCGTAATGACGGCCT')
@@ -152,12 +133,15 @@ if __name__ == "__main__":
 #Functions
 def findmotif(dnaseq, motif):
 
+#making the locations a list so i can add stuff at the end easily
     locations = []
+#turning the motif and dnaseq into lengths so it makes it easy for comparison and also accurate
     motiflen = len(motif)
     dnaseqlen = len(dnaseq)
-
+#this is me telling my code that for i in my dnaseq length, if they find something that starts with ATAT (the Motif)
     for i in range(dnaseqlen):
         if dnaseq[i:].startswith("ATAT"):
+#then they can add one to i within the locations list
             locations.append(i + 1)  
 
     return locations
